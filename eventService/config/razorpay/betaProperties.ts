179ed78821920a008c9072b00c60b9385b2f0e43
aman sharma <aman.s@pickmywork.com>
2022-11-26 17:22:36 +0530

    data

--- eventService/config/razorpay/betaProperties.ts
+++ eventService/config/razorpay/betaProperties.ts
@@ -1,7 +1,7 @@
 //represent all properties configuration pointing to beta
 module.exports = {
   RAZORPAY: {
-    key_id: "rzp_test_MkkFtprIQLTJ1C",
-    key_secret: "rdlNYtQ2IuP6xPsbeVS91hUi"
+    key_id: "rzp_test_jVa5iExtyQCEk1",
+    key_secret: "2VFDN8OChr2iFNndPY6906rQ"
   }
 };
